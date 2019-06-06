7333e9ed1d9c5d71a099df1bc46f0c5729028daf
LeeChunJun <1772043675@qq.com>
2019-06-06 15:32:52 +0800

    2019.06.06

--- wehome/src/main/java/com/licj/wehome/WebActivity.java
+++ wehome/src/main/java/com/licj/wehome/WebActivity.java
@@ -3,6 +3,7 @@ package com.licj.wehome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.tencent.smtt.sdk.WebSettings;
@@ -31,27 +32,31 @@ public class WebActivity extends AppCompatActivity {
 
         // 支持插件
         webSettings.setPluginsEnabled(true);
+        webSettings.setBlockNetworkImage(false);// 解决图片不显示
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
 
         // 设置自适应屏幕，两者合用
         webSettings.setUseWideViewPort(true); // 将图片调整到适合webview的大小
         webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
 
         // 缩放操作
-        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
-        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
-        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        webSettings.setSupportZoom(true); // 支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); // 设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); // 隐藏原生的缩放控件
 
         // 其他细节操作
-        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
-        webSettings.setAllowFileAccess(true); //设置可以访问文件
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
-        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
-        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); // 关闭webview中缓存
+        webSettings.setAllowFileAccess(true); // 设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); // 支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); // 支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");// 设置编码格式
 
         tencent_webview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
+                return true;
             }
         });
     }
